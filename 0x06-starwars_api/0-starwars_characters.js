acc38044e5ad35719b1b69ee6b68da32c2aab37e
dawit2123 <dawitzewdu2123@gmail.com>
2024-09-20 20:40:06 +0300

    Refactor starwars_characters.js to retrieve and print character names from a given movie ID

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,55 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const request = require("request");
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
-
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+if (process.argv.length !== 3) {
+  console.error("Usage: ./0-starwars_characters.js <Movie_ID>");
+  process.exit(1);
 }
 
-starwarsCharacters(filmID);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error("Error:", error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(
+      "Failed to retrieve movie information. Status Code:",
+      response.statusCode
+    );
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+  async function getCharacterData(characterUrl) {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(
+            `Failed to retrieve character information. Status Code: ${response.statusCode}`
+          );
+        } else {
+          resolve(JSON.parse(body));
+        }
+      });
+    });
+  }
+
+  (async function printCharacters() {
+    for (const characterUrl of characters) {
+      try {
+        const characterData = await getCharacterData(characterUrl);
+        console.log(characterData.name);
+      } catch (error) {
+        console.error("Error:", error);
+        process.exit(1);
+      }
+    }
+  })();
+});
